575ad034ee2a15782189c1c7ed63f81fe452edfa
Sam Odle <sro27@cornell.edu>
2020-10-17 01:45:10 -0400

    init project

---
+++ ResumeScrape/ResumeScrape/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ResumeScrape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
