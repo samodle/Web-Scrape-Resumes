97dcadb4498bf1afd64d4578c5219ae5968dc936
Sam Odle <sro27@cornell.edu>
2021-04-26 13:56:42 -0400

    addings JDs

---
+++ ResumeScrape/JobDescriptionScrape/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JobDescriptionScrape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
