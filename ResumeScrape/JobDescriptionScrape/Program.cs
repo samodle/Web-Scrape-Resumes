a329003f115c9f01719b517744a66b709471f74f
Sam Odle <sro27@cornell.edu>
2021-04-26 21:15:23 -0400

    adding JDs

---
+++ ResumeScrape/JobDescriptionScrape/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using HtmlAgilityPack;
+
+namespace JobDescriptionScrape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World..!");
+        }
+    }
+}
--- ResumeScrape/ResumeScrape/Program.cs
+++ ResumeScrape/ResumeScrape/Program.cs
@@ -1,4 +1,4 @@
-﻿using Oden.Talent;
+﻿using ShackletonJobData.Talent;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
